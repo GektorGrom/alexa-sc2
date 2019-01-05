555008cc2931fb453fdc46097d63e8118f638e58
Slava <gektorgrom@gmail.com>
2019-01-04 19:17:34 -0700

    Add timezones for event handler

--- Intents/event_search_intents.js
+++ Intents/event_search_intents.js
@@ -1,5 +1,6 @@
 import { format, addDays } from 'date-fns';
-import { zonedTimeToUtc } from 'date-fns-tz';
+import { format as formatTZ, zonedTimeToUtc, utcToZonedTime } from 'date-fns-tz';
+import axios from 'axios';
 import Speech from 'ssml-builder';
 
 import specifyDate from '../libs/Ask/specifyDate';
@@ -17,24 +18,39 @@ const EventSearchIntentHandler = {
       'EventSearchIntentHandler',
       JSON.stringify(handlerInput.requestEnvelope),
     );
+    const {
+      device: { deviceId },
+      apiAccessToken,
+      apiEndpoint,
+    } = handlerInput.requestEnvelope.context.System;
+    const tz = await axios({
+      method: 'get',
+      baseURL: apiEndpoint,
+      url: `/v2/devices/${deviceId}/settings/System.timeZone`,
+      headers: {
+        Authorization: `Bearer ${apiAccessToken}`,
+      },
+    }).then(({ data }) => data);
     const { value } = handlerInput.requestEnvelope.request.intent.slots.DATE;
     if (!value) {
       return specifyDate(handlerInput);
     }
-    const starOfDay = zonedTimeToUtc(value, 'America/New_York');
+    const starOfDay = zonedTimeToUtc(value, tz);
     const events = await getEventsByRange(
       format(starOfDay, 'T'),
       format(addDays(starOfDay, 1), 'T'),
     );
     const speech = new Speech();
     if (events.length > 0) {
-      speech.say('You can watch').pause('500ms');
-      events.forEach(({ title, stage }) => {
+      speech.say('You can watch').pauseByStrength('strong');
+      events.sort((a, b) => a.time - b.time).forEach(({ title, stage, time }) => {
         speech
           .emphasis('moderate', title)
           .pause('150ms')
           .say(stage)
-          .pause('750ms');
+          .pauseByStrength('weak')
+          .say(`at ${formatTZ(utcToZonedTime(time, tz), 'hh:mm a', { timeZone: tz })}`)
+          .pauseByStrength('x-strong');
       });
     } else {
       speech.say('No events for this day');
